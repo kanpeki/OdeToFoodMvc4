6e16c600c0ffbfdabd7f4d29e8f294e9373dc991
Iulia Sargu <i.sargu@levi9.com>
2017-08-28 13:36:51 +0300

    added initial project files (to make it work change connection string, copy configuration file (seed methods) in migrations, delete migrations, run commands in PMC - Enable-Migrations, replace configuration file, add-migration "initialconfig", update-database)

---
+++ OdeToFood/Models/OdeToFoodDb.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+
+namespace OdeToFood.Models
+{
+	public interface IOdeToFoodDb : IDisposable
+    {
+        IQueryable<T> Query<T>() where T : class;
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Remove<T>(T entity) where T : class;
+        void SaveChanges();
+    }
+
+
+    public class OdeToFoodDb : DbContext, IOdeToFoodDb
+    {
+        public OdeToFoodDb() : base("name=DefaultConnection")
+        {
+
+        }
+
+        public DbSet<UserProfile> UserProfiles { get; set; }
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<RestaurantReview> Reviews { get; set; }
+
+        IQueryable<T> IOdeToFoodDb.Query<T>()
+        {
+            return Set<T>();
+        }
+
+        void IOdeToFoodDb.Add<T>(T entity)
+        {
+            Set<T>().Add(entity);
+        }
+
+        void IOdeToFoodDb.Update<T>(T entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+
+        void IOdeToFoodDb.Remove<T>(T entity)
+        {
+            Set<T>().Remove(entity);
+        }
+
+        void IOdeToFoodDb.SaveChanges()
+        {
+            SaveChanges();
+        }
+    }
+}
